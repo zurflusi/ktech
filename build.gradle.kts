8906a528eca5ba664ed18259eb26082612044c08
Simon Zurfluh <simon.zurfluh@ergon.ch>
2023-08-25 22:28:12 +0200

    add openapi generator version 6.6.0 and first example openapi.json

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.1.3"
     id("io.spring.dependency-management") version "1.1.3"
+    id("org.openapi.generator") version "6.6.0"
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
 }
@@ -34,6 +35,26 @@ tasks.withType<KotlinCompile> {
         freeCompilerArgs += "-Xjsr305=strict"
         jvmTarget = "17"
     }
+    dependsOn(
+        tasks.named("openApiValidate"),
+        tasks.named("openApiGenerate"),
+    )
+}
+
+openApiGenerate {
+    generatorName.set("kotlin-spring")
+    inputSpec.set("$rootDir/src/main/resources/specs/openapi.json")
+    configOptions.set(
+        mutableMapOf(
+            "serializationLibrary" to "kotlinx_serialization",
+            "useTags" to "true",
+        ),
+    )
+}
+
+openApiValidate {
+    inputSpec.set("$rootDir/src/main/resources/specs/openapi.json")
+    recommend.set(true)
 }
 
 tasks.withType<Test> {
